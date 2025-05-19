ae77255f1d5b3230a5befaff15b188becdc59e33
SDG-24738 <shashankvedantr@gmail.com>
2025-05-19 19:41:55 +0530

    Bitwise

---
+++ bitwise.py
@@ -0,0 +1,8 @@
+a = 10
+b = -10
+print("Bitwise right-shift operator")
+print("a >> 1 = ", a >> 1)
+print("b >> 1 = ", b >> 1)
+print("Bitwise left-shift operator")
+print("a << 1 = ", a << 1)
+print("b << 1 = ", b << 1)
