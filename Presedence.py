1dc14b94260dde13308bfc8abf5539f18d187354
SDG-24738 <shashankvedantr@gmail.com>
2025-05-26 19:15:40 +0530

    Precedence

---
+++ Presedence.py
@@ -0,0 +1,11 @@
+print("Operators precedence in python")
+exp1 = 10 + 5 * 2
+print(exp1)
+name = "Shashank"
+age = 15
+if name == "Shashank" or name == "Sonali" and age < 20:
+    print("Hello")
+else:
+    print("Bye Bye")
+exp2 = 100/10 * 40
+print(exp2)
