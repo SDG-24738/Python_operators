67dfdd043f0a5eccc88994693b75a587e430df60
SDG-24738 <shashankvedantr@gmail.com>
2025-05-26 19:34:50 +0530

    AVERAGE

---
+++ averageee.py
@@ -0,0 +1,10 @@
+cycle1 = 10
+cycle2 = 20
+cycle3 = 30
+average = (10 + 20 + 30)/3
+if average > cycle1:
+    print("cycle1 is slower than average speed")
+elif average  > cycle2:
+    print("Cycle2 is solwer than average speed")
+else:
+    print("cycle3 is slower than average speed")
